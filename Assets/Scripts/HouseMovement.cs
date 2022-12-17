7f360c1785f952ecbfd7a50557e65908325308cc
ErikRadoan <78492289+ErikRadoan@users.noreply.github.com>
2022-12-17 15:46:42 +0000

    SmoothMovment

--- Assets/Scripts/HouseMovement.cs
+++ Assets/Scripts/HouseMovement.cs
@@ -7,6 +7,7 @@ public class HouseMovement : MonoBehaviour
 {
     private Transform _transform;
     [SerializeField] private float moveAmount;
+    [SerializeField] private float startTime;
     private void Awake()
     {
         _transform = gameObject.transform;
@@ -21,7 +22,7 @@ public class HouseMovement : MonoBehaviour
 
     private IEnumerator Move()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(startTime);
         gameObject.transform.position = new Vector2(gameObject.transform.position.x, _transform.position.y - moveAmount);
         StartCoroutine("Move");
     }

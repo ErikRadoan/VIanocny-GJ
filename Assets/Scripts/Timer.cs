449e3b49dde7b6673cd2199abedbd1a02f5d6e04
Emma <emma.simkova123@gmail.com>
2022-12-17 14:33:31 +0100

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 60;
+    public bool timerIsRunning = false;
+    public TMPro.TextMeshProUGUI timerText;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}

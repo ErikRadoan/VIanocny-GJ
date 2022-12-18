360c409129f4c2b20c048404e29429d2c4590607
Killgris <alex.kosco123@gmail.com>
2022-12-18 10:43:01 +0100

    particle system fix

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void RetryGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
     
     

c376e05d4e1850b60d0c064c409686fa622a598f
ErikRadoan <78492289+ErikRadoan@users.noreply.github.com>
2022-12-18 13:23:11 +0000

    HouseLost

---
+++ Assets/Scripts/HouseLost.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseLost : MonoBehaviour
+{
+    private void Update()
+    {
+        if (gameObject.transform.position.y < -6)
+        {
+            //LOst
+        }
+    }
+}
